d9aa0f668441920b31a34f1bdb73333adaac16e7
Adrian Poniatowski <66631454+Adrian235713@users.noreply.github.com>
2022-04-08 09:46:47 +0200

    Troll - porawiony
    Troll - porawiony

--- EVENTS/Events.py
+++ EVENTS/Events.py
@@ -10,14 +10,14 @@ class Events():
     def envents(self, player):
 
         if player.level == 2 and self.wygrana_w_karty == False:
-            self.wygrana_w_karty == True
+            self.wygrana_w_karty = True
             player.goold += 50
             print("<>==<>"*10)
             print("\t \t \t \tSpotykasz i ogrywasz Trolla w karty -Zyskujesz 50 sztuk złota ")
             print("<>==<>"*10)
 
         if player.level == 3 and self.spotkanie_uzdrowiciela == False:
-            self.spotkanie_uzdrowiciela == True
+            self.spotkanie_uzdrowiciela = True
             player.hp = player.max_hp
             player.goold += 50
             print("<>==<>"*10)
